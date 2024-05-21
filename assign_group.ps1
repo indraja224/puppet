3e905cb75c289cf3be8b6c0419e4d36ae080d8df
indraja <indrajap088@gmail.com>
2024-05-21 10:56:53 +0000

    version 1 of the script

---
+++ assign_group.ps1
@@ -0,0 +1,50 @@
+#!/usr/bin/env pwsh
+[CmdletBinding()]
+Param(
+  [Parameter(Mandatory = $True)]
+  [String[]] $servers,
+  [Parameter(Mandatory = $True)]
+  [String] $groups,
+  [Parameter(Mandatory = $True)]
+  [String] $cyber_arc_account
+
+)
+$cyber_arc_account=$cyber_arc_account+"*"
+$shared_accounts_list=(Get-ADUser -Filter {Name -like $cyber_arc_account}).name
+ function assign_admin($srv){
+$grp="srv_"+$srv+"_Admin"
+if( Get-ADGroup -Filter {Name -eq $grp}){
+foreach ($ac in $shared_accounts_list){
+Add-ADGroupMember -Identity $grp -Members $ac
+write-host "$ac added to $grp"
+}
+}
+else{
+write-host "the Group $grp does not exist"
+}
+}
+
+function assign_rdp($srv){
+$grp="srv_"+$srv+"_rdp"
+if( Get-ADGroup -Filter {Name -eq $grp}){
+foreach ($ac in $shared_accounts_list){
+Add-ADGroupMember -Identity $grp -Members $ac
+write-host "$ac added to $grp"
+}
+}
+else{
+write-host "the Group $grp does not exist"
+}
+
+}
+
+
+foreach($srv in $servers){
+if($groups -eq "admin"){
+assign_admin -srv $srv
+}
+if($groups -eq "rdp"){
+assign_rdp -srv $srv
+}
+
+}
