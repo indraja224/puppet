1dc6ec840f6a10dc2537aa7b451f88a1d1bd1120
indraja <indrajap088@gmail.com>
2024-05-29 11:05:35 +0000

    "changed bu adding -server $DC param"

--- assign_group.ps1
+++ assign_group.ps1
@@ -6,17 +6,20 @@ Param(
   [Parameter(Mandatory = $True)]
   [String[]] $groups,
   [Parameter(Mandatory = $True)]
-  [String] $cyber_arc_account
+  [String] $cyber_arc_account,
+  [Parameter(Mandatory = $True)]
+  [String] $domain
+
 
 )
 $serverNamePattern = "^[a-zA-Z0-9-]+$"
 $cyber_arc_account=$cyber_arc_account+"*"
-$shared_accounts_list=(Get-ADUser -Filter {Name -like $cyber_arc_account}).name
+$shared_accounts_list=(Get-ADUser -Filter {Name -like $cyber_arc_account} -Server $domain ).name
 function assign_admin($srv){
 $grp="srv_"+$srv+"_Admin"
-if( Get-ADGroup -Filter {Name -eq $grp}){
+if( Get-ADGroup -Filter {Name -eq $grp} -Server $domain){
 foreach ($ac in $shared_accounts_list){
-if((get-ADGroupMember -Identity $grp).name -ccontains $ac){
+if((get-ADGroupMember -Identity $grp -Server $domain).name -ccontains $ac){
 write-host "$ac already exists in group $grp"
 }
 else{
@@ -34,9 +37,9 @@ write-host "the Group $grp does not exist"
 }
 function assign_rdp($srv){
 $grp="srv_"+$srv+"_rdp"
-if( Get-ADGroup -Filter {Name -eq $grp}){
+if( Get-ADGroup -Filter {Name -eq $grp} -Server $domain){
 foreach ($ac in $shared_accounts_list){
-if((get-ADGroupMember -Identity $grp).name -ccontains $ac){
+if((get-ADGroupMember -Identity $grp -Server $domain).name -ccontains $ac){
 write-host "$ac already exists in group $grp"
 }
 else{
